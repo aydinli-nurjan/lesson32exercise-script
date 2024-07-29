dfe8dbe2ab7e5cc628cb09a6d4e7dec40980f540
Khadija Aydinli <khadijaaydinligmail.com@Khadijas-MacBook-Air.local>
2024-07-30 01:27:59 +0400

    first

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const school = [
+    ['Nurcan', ['Elaci'], 'Aysel'],
+    ['Ibrahim', 'Xaliq ve Murad'],
+    ['8R', '9B']
+]
+
+let olkeler = [
+    ['Azerbaijan', 'Turkiye'],
+    ['Almaniya', 'Fransa', 'Holland', 'Qatar']
+]
+
+console.log(`${school[0][0][0]}${olkeler[0][1][1]}${school[1][0][2]}${olkeler[0][0][7]}${school[1][1][1]}${olkeler[1][2][5]} ${school[0][2][0]}${olkeler[1][0][6]}${olkeler[1][2][6]}${olkeler[0][0][6]}${olkeler[0][0][9]}${olkeler[1][2][2]}${olkeler[0][0][6]} ${olkeler[1][3][0]}${olkeler[1][0][3]}${olkeler[1][1][0]}${olkeler[0][1][1]}${olkeler[1][1][1]} ${school[1][1][4]}${olkeler[0][0][6]}${olkeler[0][0][1]}${olkeler[0][0][6]}`)
