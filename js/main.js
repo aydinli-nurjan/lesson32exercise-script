4360c9f4243ca7f858ccff134e3234794e3e3ebe
Khadija Aydinli <khadijaaydinligmail.com@Khadijas-MacBook-Air.local>
2024-07-30 15:02:19 +0400

    first

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const school = [
     ['Nurcan', ['Elaci'], 'Aysel'],
-    ['Ibrahim', 'Xaliq ve Murad'],
+    ['Ibrahim', 'Xaliq ve Murad', 'Qafur'],
     ['8R', '9B']
 ]
 
@@ -9,4 +9,4 @@ let olkeler = [
     ['Almaniya', 'Fransa', 'Holland', 'Qatar']
 ]
 
-console.log(`${school[0][0][0]}${olkeler[0][1][1]}${school[1][0][2]}${olkeler[0][0][7]}${school[1][1][1]}${olkeler[1][2][5]} ${school[0][2][0]}${olkeler[1][0][6]}${olkeler[1][2][6]}${olkeler[0][0][6]}${olkeler[0][0][9]}${olkeler[1][2][2]}${olkeler[0][0][6]} ${olkeler[1][3][0]}${olkeler[1][0][3]}${olkeler[1][1][0]}${olkeler[0][1][1]}${olkeler[1][1][1]} ${school[1][1][4]}${olkeler[0][0][6]}${olkeler[0][0][1]}${olkeler[0][0][6]}`)
+console.log(`${school[0][0][0]}${olkeler[0][1][1]}${school[1][0][2]}${olkeler[0][0][7]}${school[1][1][1]}${olkeler[1][2][5]} ${school[0][2][0]}${olkeler[1][0][6]}${olkeler[1][2][6]}${olkeler[0][0][6]}${olkeler[0][0][9]}${olkeler[1][2][2]}${olkeler[0][0][6]} ${olkeler[1][3][0]}${olkeler[1][0][3]}${school[1][2][2]}${olkeler[0][1][1]}${olkeler[1][1][1]} ${school[1][1][4]}${olkeler[0][0][6]}${olkeler[0][0][1]}${olkeler[0][0][6]}`)
